a61c6714035f34c5063b2e876013359cc464cd60
arhamamir810 <arhamamir810@gmail.com>
2026-07-19 00:57:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,59 +7,36 @@ const noBtn = document.getElementById("noBtn");
 
 const question = document.getElementById("question");
 
+let index = 0;
 
 let questions = [
-
-"Do you think we can smile again? 😊",
-
-"Can we forget the small mistake and keep good memories? ❤️",
-
-"Will you give me a chance to make things better? 🧸",
-
-"Can we be friends like before? 💕",
-
-"One more try? 🥺"
-
+    "Do you think we can smile again? 😊",
+    "Can we keep our good memories and move forward? ❤️",
+    "Will you give me a chance to make you smile again? 🧸",
+    "Can we be like before? 💕",
+    "One more chance? 🥺"
 ];
 
 
-let index = 0;
-
-
-startBtn.onclick = function(){
-
-    popup.style.display="flex";
-
+startBtn.onclick = function () {
+    popup.style.display = "flex";
 };
 
 
-
-yesBtn.onclick=function(){
-
-    popup.style.display="none";
-    final.style.display="flex";
-
+yesBtn.onclick = function () {
+    popup.style.display = "none";
+    final.style.display = "flex";
 };
 
 
-
-noBtn.onclick=function(){
+noBtn.onclick = function () {
 
     index++;
 
-    if(index < questions.length){
-
-        question.innerHTML = questions[index];
-
-    }
-
-    else {
-
-        question.innerHTML =
-        "I will keep trying to make you smile 😊";
-
+    if (index >= questions.length) {
         index = 0;
-
     }
+
+    question.innerHTML = questions[index];
 
 };

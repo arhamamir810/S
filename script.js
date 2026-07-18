55012a738dbcb5020df7811c1929f7c59653da8a
arhamamir810 <arhamamir810@gmail.com>
2026-07-19 00:44:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,22 @@
-body {
-    margin: 0;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: #ffd6e7;
-    font-family: Arial, sans-serif;
-}
+const popup = document.getElementById("popup");
+const thanks = document.getElementById("thanks");
 
-.container {
-    text-align: center;
-    background: white;
-    padding: 30px;
-    border-radius: 20px;
-    width: 300px;
-}
+const nextBtn = document.getElementById("nextBtn");
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
 
-.teddy {
-    font-size: 80px;
-}
 
-h1 {
-    color: #ff4d88;
-}
+nextBtn.onclick = function() {
+    popup.style.display = "flex";
+};
 
-button {
-    padding: 12px 25px;
-    border: none;
-    border-radius: 20px;
-    background: #ff4d88;
-    color: white;
-    cursor: pointer;
-    margin: 10px;
-}
 
-.popup {
-    display: none;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
-    background: rgba(0,0,0,0.4);
-}
+yesBtn.onclick = function() {
+    popup.style.display = "none";
+    thanks.style.display = "flex";
+};
 
-.box {
-    background: white;
-    padding: 30px;
-    border-radius: 20px;
-    text-align: center;
-}
+
+noBtn.onclick = function() {
+    alert("I will wait for your forgiveness 🥺");
+};

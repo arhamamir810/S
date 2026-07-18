94b16ca9017af2372349e2f716fa2e3a6d3927f9
arhamamir810 <arhamamir810@gmail.com>
2026-07-19 00:52:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
+const startBtn = document.getElementById("startBtn");
 const popup = document.getElementById("popup");
-const thanks = document.getElementById("thanks");
+const final = document.getElementById("final");
 
-const nextBtn = document.getElementById("nextBtn");
-const yesBtn = document.getElementById("yes");
-const noBtn = document.getElementById("no");
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+const question = document.getElementById("question");
 
 
-nextBtn.onclick = function() {
-    popup.style.display = "flex";
+let questions = [
+
+"Do you think we can smile again? 😊",
+
+"Can we forget the small mistake and keep good memories? ❤️",
+
+"Will you give me a chance to make things better? 🧸",
+
+"Can we be friends like before? 💕",
+
+"One more try? 🥺"
+
+];
+
+
+let index = 0;
+
+
+startBtn.onclick = function(){
+
+    popup.style.display="flex";
+
 };
 
 
-yesBtn.onclick = function() {
-    popup.style.display = "none";
-    thanks.style.display = "flex";
+
+yesBtn.onclick=function(){
+
+    popup.style.display="none";
+    final.style.display="flex";
+
 };
 
 
-noBtn.onclick = function() {
-    alert("I will wait for your forgiveness 🥺");
+
+noBtn.onclick=function(){
+
+    index++;
+
+    if(index < questions.length){
+
+        question.innerHTML = questions[index];
+
+    }
+
+    else {
+
+        question.innerHTML =
+        "I will keep trying to make you smile 😊";
+
+        index = 0;
+
+    }
+
 };
